d6db6df12451db659af54f1c016acbc8f4a31f40
Ambika Ambekar <125361886+ambekarambika@users.noreply.github.com>
2026-06-20 09:24:31 +0530

    Add new CORS origin for production deployment

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "http://127.0.0.1:8080"],
+    allow_origins=["http://localhost:8080", "http://127.0.0.1:8080","https://prop-manager-premium-rental-managem.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
